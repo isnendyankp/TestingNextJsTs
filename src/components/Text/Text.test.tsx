34bc6db007536dc3a07e74b8c5714fa3efad7be6
isnendyankp <isnendyankp@gmail.com>
2024-01-26 15:42:32 +0700

    Add screen.getByText() to Text component test

--- src/components/Text/Text.test.tsx
+++ src/components/Text/Text.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe } from 'node:test';
 import Text from '.';
 import React from 'react';
@@ -7,7 +7,7 @@ describe('Unit test for Text Component', () => {
    
     test('component should be return text', () => {
         render (<Text>{'testing text'}</Text>);
-
-        console.log('testing text');
+        const element = screen.getByText('testing text');
+        expect(element).toMatchSnapshot();
     });
 });
